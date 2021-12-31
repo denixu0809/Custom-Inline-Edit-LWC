fd7e3e3afd0eb85f337f61a87a5b9c28743b7f91
denixu0809 <divyeshdudhats@gmail.com>
2021-12-31 18:46:50 +0530

    made some changes in custom lookup and add targets for component

--- force-app/main/default/lwc/customLookupLwc/customLookupLwc.js
+++ force-app/main/default/lwc/customLookupLwc/customLookupLwc.js
@@ -1,18 +1,21 @@
-import { LightningElement,api,wire} from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
 // import apex method from salesforce module 
 import fetchLookupData from '@salesforce/apex/CustomLookupLwcController.fetchLookupData';
 import fetchDefaultRecord from '@salesforce/apex/CustomLookupLwcController.fetchDefaultRecord';
+import getNameField from '@salesforce/apex/CustomLookupLwcController.getNameField';
 const DELAY = 300; // dealy apex callout timing in miliseconds  
 export default class CustomLookupLwc extends LightningElement {
     // public properties with initial default values 
     @api label;
-    @api placeholder = 'search...'; 
+    @api placeholder = 'search...';
     @api iconName = 'standard:account';
     @api sObjectApiName = 'Account';
     @api defaultRecordId = '';
+    @api nameField;
 
-    get isLabelAvailable(){
-        if(this.label == undefined){
+
+    get isLabelAvailable() {
+        if (this.label == undefined) {
             return false;
         }
         return true;
@@ -21,17 +24,37 @@ export default class CustomLookupLwc extends LightningElement {
 
     // private properties 
     lstResult = []; // to store list of returned records   
-    hasRecords = true; 
-    searchKey=''; // to store input field value    
+    hasRecords = true;
+    searchKey = ''; // to store input field value    
     isSearchLoading = false; // to control loading spinner  
     delayTimeout;
     selectedRecord = {}; // to store selected lookup record in object formate 
-   // initial function to populate default selected lookup record if defaultRecordId provided  
-    connectedCallback(){
-         if(this.defaultRecordId != ''){
-            fetchDefaultRecord({ recordId: this.defaultRecordId , 'sObjectApiName' : this.sObjectApiName })
+    // initial function to populate default selected lookup record if defaultRecordId provided  
+    connectedCallback() {
+
+        if (this.nameField == undefined) {
+            getNameField({ 'objectAPIName': this.sObjectApiName })
+                .then((result) => {
+                    if (result != null) {
+                        this.nameField = result;
+                        this.fetchRecord();
+                    }
+                })
+                .catch((error) => {
+                });
+        }
+
+        if (this.defaultRecordId != '' && (this.selectedRecord == undefined || this.selectedRecord == {} || this.selectedRecord.Id == undefined)) {
+            this.fetchRecord();
+        } else {
+            this.handelSelectRecordHelper();
+        }
+    }
+
+    fetchRecord() {
+        fetchDefaultRecord({ recordId: this.defaultRecordId, 'sObjectApiName': this.sObjectApiName, nameField: this.nameField })
             .then((result) => {
-                if(result != null){
+                if (result != null) {
                     this.selectedRecord = result;
                     this.handelSelectRecordHelper(); // helper function to show/hide lookup result container on UI
                 }
@@ -40,23 +63,29 @@ export default class CustomLookupLwc extends LightningElement {
                 this.error = error;
                 this.selectedRecord = {};
             });
-         }
+    }
+
+    get pillLabel() {
+        if (this.selectedRecord != undefined && this.selectedRecord.Id != undefined && this.nameField != undefined) {
+            return this.selectedRecord[this.nameField];
+        }
+        return null;
     }
     // wire function property to fetch search record based on user input
-    @wire(fetchLookupData, { searchKey: '$searchKey' , sObjectApiName : '$sObjectApiName' })
-     searchResult(value) {
+    @wire(fetchLookupData, { searchKey: '$searchKey', sObjectApiName: '$sObjectApiName', nameField: '$nameField' })
+    searchResult(value) {
         const { data, error } = value; // destructure the provisioned value
         this.isSearchLoading = false;
         if (data) {
-             this.hasRecords = data.length == 0 ? false : true; 
-             this.lstResult = JSON.parse(JSON.stringify(data)); 
-         }
+            this.hasRecords = data.length == 0 ? false : true;
+            this.lstResult = JSON.parse(JSON.stringify(data));
+        }
         else if (error) {
             console.log('(error---> ' + JSON.stringify(error));
-         }
+        }
     };
-       
-  // update searchKey property on input field change  
+
+    // update searchKey property on input field change  
     handleKeyChange(event) {
         // Debouncing this method: Do not update the reactive property as long as this function is
         // being called within a delay of DELAY. This is to avoid a very large number of Apex method calls.
@@ -64,61 +93,61 @@ export default class CustomLookupLwc extends LightningElement {
         window.clearTimeout(this.delayTimeout);
         const searchKey = event.target.value;
         this.delayTimeout = setTimeout(() => {
-        this.searchKey = searchKey;
+            this.searchKey = searchKey;
         }, DELAY);
     }
     // method to toggle lookup result section on UI 
-    toggleResult(event){
+    toggleResult(event) {
         const lookupInputContainer = this.template.querySelector('.lookupInputContainer');
         const clsList = lookupInputContainer.classList;
         const whichEvent = event.target.getAttribute('data-source');
-        switch(whichEvent) {
+        switch (whichEvent) {
             case 'searchInputField':
                 clsList.add('slds-is-open');
-               break;
+                break;
             case 'lookupContainer':
-                clsList.remove('slds-is-open');    
-            break;                    
-           }
+                clsList.remove('slds-is-open');
+                break;
+        }
     }
-   // method to clear selected lookup record  
-   handleRemove(){
-    this.searchKey = '';    
-    this.selectedRecord = {};
-    this.lookupUpdatehandler(undefined); // update value on parent component as well from helper function 
-    
-    // remove selected pill and display input field again 
-    const searchBoxWrapper = this.template.querySelector('.searchBoxWrapper');
-     searchBoxWrapper.classList.remove('slds-hide');
-     searchBoxWrapper.classList.add('slds-show');
-     const pillDiv = this.template.querySelector('.pillDiv');
-     pillDiv.classList.remove('slds-show');
-     pillDiv.classList.add('slds-hide');
-  }
-  // method to update selected record from search result 
-handelSelectedRecord(event){   
-     var objId = event.target.getAttribute('data-recid'); // get selected record Id 
-     this.selectedRecord = this.lstResult.find(data => data.Id === objId); // find selected record from list 
-     this.lookupUpdatehandler(this.selectedRecord); // update value on parent component as well from helper function 
-     this.handelSelectRecordHelper(); // helper function to show/hide lookup result container on UI
-}
-/*COMMON HELPER METHOD STARTED*/
-handelSelectRecordHelper(){
-    this.template.querySelector('.lookupInputContainer').classList.remove('slds-is-open');
-     const searchBoxWrapper = this.template.querySelector('.searchBoxWrapper');
-     searchBoxWrapper.classList.remove('slds-show');
-     searchBoxWrapper.classList.add('slds-hide');
-     const pillDiv = this.template.querySelector('.pillDiv');
-     pillDiv.classList.remove('slds-hide');
-     pillDiv.classList.add('slds-show');     
-}
-// send selected lookup record to parent component using custom event
-lookupUpdatehandler(value){    
-    const oEvent = new CustomEvent('lookupupdate',
-    {
-        'detail': {selectedRecord: value}
+    // method to clear selected lookup record  
+    handleRemove() {
+        this.searchKey = '';
+        this.selectedRecord = {};
+        this.lookupUpdatehandler(undefined); // update value on parent component as well from helper function 
+
+        // remove selected pill and display input field again 
+        const searchBoxWrapper = this.template.querySelector('.searchBoxWrapper');
+        searchBoxWrapper.classList.remove('slds-hide');
+        searchBoxWrapper.classList.add('slds-show');
+        const pillDiv = this.template.querySelector('.pillDiv');
+        pillDiv.classList.remove('slds-show');
+        pillDiv.classList.add('slds-hide');
     }
-);
-this.dispatchEvent(oEvent);
-}
+
+    onSelectRecordHandler(event) {
+        var selectedRecord = JSON.parse(event.detail);
+        this.lookupUpdatehandler(selectedRecord); // update value on parent component as well from helper function 
+        this.handelSelectRecordHelper(); // helper function to show/hide lookup result container on UI
+    }
+
+    /*COMMON HELPER METHOD STARTED*/
+    handelSelectRecordHelper() {
+        this.template.querySelector('.lookupInputContainer').classList.remove('slds-is-open');
+        const searchBoxWrapper = this.template.querySelector('.searchBoxWrapper');
+        searchBoxWrapper.classList.remove('slds-show');
+        searchBoxWrapper.classList.add('slds-hide');
+        const pillDiv = this.template.querySelector('.pillDiv');
+        pillDiv.classList.remove('slds-hide');
+        pillDiv.classList.add('slds-show');
+    }
+    // send selected lookup record to parent component using custom event
+    lookupUpdatehandler(value) {
+        const oEvent = new CustomEvent('lookupupdate',
+            {
+                'detail': { selectedRecord: value }
+            }
+        );
+        this.dispatchEvent(oEvent);
+    }
 }
